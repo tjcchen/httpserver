4a9b60efcd9b76c275d564db1dea686cb61259bf
Andy Chen <chenyangpoppin@hotmail.com>
2023-05-09 18:57:49 +0800

    feat: performance profiling

---
+++ test/pprof.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+func main() {
+	fmt.Println("===Performance Profiling Starts===")
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	fmt.Println("===Performance Profiling Ends===")
+}
