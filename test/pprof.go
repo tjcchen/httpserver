a9cf52c7eb6051febae64b1dc7dc345360979919
Andy Chen <chenyangpoppin@hotmail.com>
2023-05-10 10:26:48 +0800

    feat: other profiling tools

--- test/pprof.go
+++ test/pprof.go
@@ -10,6 +10,20 @@ import (
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
+/**
+ * Analyze CPU Profile:
+ * 1. generate report to a specific location, like /tmp/cpuprofile
+ * 2. run command `go tool pprof /tmp/cpuprofile`
+ * 3. check top 10 threads by running `top 10`
+ * 4. (pprof) list main.main - to display more info( optional )
+ * 5. to generate a svg report
+ *
+ * Other profiling tools:
+ * - CPU profiling( CPU usage, per 100ms )
+ * - Memory profiling( memory usage )
+ * - Block profiling( analyze deadlock )
+ * - Goroutine profiling( goroutine information, scheduling relationship )
+ */
 func main() {
 	fmt.Println("===Performance Profiling Starts===")
 	flag.Parse()
